348e68ebbcb2fd563a1ec11c5904caab070a6ba9
Moataz Gamal <moatazgamal@MacBook-Pro-alkhas-b-Moataz.local>
2022-02-26 19:56:50 +0200

    third comment for images

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let accordion =document.getElementsByClassName('accordion');
 let desc =document.getElementsByClassName('desc');
 
 
+
 for(let i=0; i<desc.length; i++){
 
     accordion[i].addEventListener('click',function(){
@@ -18,3 +19,21 @@ for(let i=0; i<desc.length; i++){
 
 }
 
+let thumbanilimg =document.querySelectorAll('.thumbanil img');
+let previewimg =document.querySelector('.preview img');
+
+
+
+
+for(let i = 0; i<thumbanilimg.length; i++){ 
+
+
+thumbanilimg[i].addEventListener('click',function(){
+
+
+    previewimg.src = thumbanilimg[i].src
+
+
+
+})
+}
